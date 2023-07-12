36ca210108121f76b9008435ce50e7a9a87aa293
Antonio <antonio.tabares@outlook.es>
2023-07-12 14:19:22 -0500

    Attempt n°6

--- lab_app/lab_app/settings.py
+++ lab_app/lab_app/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['labapp-j8cm.onrender.com', 'localhost']
 
@@ -137,6 +137,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticfiles')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
